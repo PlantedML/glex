52606ee61ce405cbb048399e8a76be7aec04488e
Lukas Burk <lukas@quantenbrot.de>
2023-02-27 11:39:48 +0100

    glex_vi + autoplot method

--- R/glex_vi.R
+++ R/glex_vi.R
@@ -2,7 +2,7 @@
 #'
 #' @param object Object of class `glex`.
 #' @param threshold (`numeric(1)`) Optional threshold to filter output to include only importance scores greater
-#'   than this value. Refers to percentage of average absolute `m` divided by average prediction (`intercept`).
+#'   than this value. Refers to `m_rel`, the average absolute `m` divided by average prediction (`intercept`).
 #' @param ... (Unused)
 #'
 #' @return A data.table with columns:
@@ -11,7 +11,7 @@
 #' * `term_list` (`list`): Same as `term` but as a list-column to enable filtering by specific variables without
 #'   requiring to split by `:`.
 #' * `m` (`numeric`): Average absolute contribution of `term`, see Details.
-#' * `m_rel` (`numeric`): `m` but relative to the average prediction (`intercept` in `glex()` output), times 100.
+#' * `m_rel` (`numeric`): `m` but relative to the average prediction (`intercept` in `glex()` output).
 #'
 #' @export
 #'
@@ -22,8 +22,9 @@
 #'
 #' In turn, `m_rel` rescales `m` by the average prediction of the model (\eqn{m_0}, `intercept` as reported by `glex()`):
 #'
-#' \deqn{\mathtt{m\_rel} = \frac{\mathtt{m}}{m_0} \cdot 100}
+#' \deqn{\mathtt{m\_rel} = \frac{\mathtt{m}}{m_0}}
 #'
+#' @seealso autoplot.glex_vi
 #' @examples
 #' # Random Planted Forest -----
 #' if (requireNamespace("randomPlantedForest", quietly = TRUE)) {
@@ -37,11 +38,12 @@
 #' vi_rpf <- glex_vi(glex_rpf)
 #'
 #' # Only terms with a relative contribution greater or equal 0.1% of the average prediction
-#' vi_rpf_filtered <- glex_vi(glex_rpf, threshold = 0.1)
+#' vi_rpf_filtered <- glex_vi(glex_rpf, threshold = 0.001)
 #'
 #' library(ggplot2)
 #' autoplot(vi_rpf_filtered)
 #' autoplot(vi_rpf, by_degree = TRUE)
+#' autoplot(vi_rpf, scale = "relative", threshold = 0.005)
 #' }
 #'
 #' # xgboost -----
@@ -58,6 +60,7 @@
 #' library(ggplot2)
 #' autoplot(vi_xgb)
 #' autoplot(vi_xgb, by_degree = TRUE)
+#' autoplot(vi_xgb, scale = "relative")
 #' }
 glex_vi <- function(object, threshold = 0, ...) {
   checkmate::assert_class(object, classes = "glex")
@@ -68,68 +71,20 @@ glex_vi <- function(object, threshold = 0, ...) {
 
   xcomp <- data.table::copy(object$m)
   intercept <- object$intercept
-  pred <- rowSums(xcomp) + intercept
-
-  xcomp[, ".id" := seq_len(.N)]
+  # pred <- rowSums(xcomp) + intercept
 
   # longify for extraction
-  m_long <- melt(xcomp, id.vars = ".id", value.name = "m", variable.name = "term", variable.factor = FALSE)
+  xcomp[, ".id" := seq_len(.N)]
+  m_long <- data.table::melt(xcomp, id.vars = ".id", value.name = "m",
+                             variable.name = "term", variable.factor = FALSE)
 
   m_aggr <- m_long[, list(m = mean(abs(m))), by = "term"]
   m_aggr[, term_list := lapply(term, function(x) unlist(strsplit(x, ":")))]
   m_aggr[, degree := vapply(term_list, length, integer(1))]
-  m_aggr[, m_rel := 100 * (m / ..intercept)]
+  m_aggr[, m_rel := (m / ..intercept)]
 
   data.table::setorder(m_aggr, -m)
   data.table::setcolorder(m_aggr, c("degree", "term", "term_list", "m", "m_rel"))
   class(m_aggr) <- c("glex_vi", class(m_aggr))
   m_aggr[m_rel >= threshold]
-}
-
-
-#' Plot glex Variable Importances
-#'
-#' @param object Object of class `glex_vi`, see [`glex_vi()`].
-#' @param by_degree (`FALSE`) Optionally sum values by degree of interaction, resulting in one contribution score
-#'  for all main effects, all second-order interactions, etc.
-#' @param ... (Unused)
-#'
-#' @return A `ggplot2` object.
-#' @export
-autoplot.glex_vi <- function(object, by_degree = FALSE, ...) {
-
-  checkmate::assert_flag(by_degree)
-  # object <- data.table::copy(object)
-  by_what <- ifelse(by_degree, "Degree", "Term")
-
-  # FIXME: data.table NSE stuff
-  m <- NULL
-
-  if (by_what == "Degree") {
-
-    aggr <- object[, list(m = sum(m)), by = "degree"]
-
-    p <- ggplot(aggr)
-    p <- p + aes(y = stats::reorder(.data[["degree"]], .data[["m"]]))
-    x_lab <- "Sum of Average Absolute Contributions"
-
-  }
-
-  if (by_what == "Term") {
-
-    p <- ggplot(object)
-    p <- p + aes(y = stats::reorder(.data[["term"]], .data[["m"]]))
-    x_lab <- "Average Absolute Contribution"
-
-  }
-
-  p + aes(x = .data[["m"]], fill = factor(.data[["degree"]])) +
-    geom_col(linewidth = 0, alpha = .8) +
-    scale_fill_viridis_d(direction = -1, end = .95) +
-    labs(
-      title = sprintf("Average Absolute Prediction Contribution by %s", by_what),
-      y = by_what, x = x_lab,
-      fill = "Degree of Interaction"
-    ) +
-    theme(legend.position = "bottom")
 }
