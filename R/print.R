601d34655e6d61f9185b26c92dcd70490a3d5ad2
Lukas Burk <lukas@quantenbrot.de>
2023-03-06 00:39:38 +0100

    Add example to print.glex just in case

--- R/print.R
+++ R/print.R
@@ -8,14 +8,21 @@
 #'
 #' @export
 #' @importFrom utils str
+#' @examples
+#' # Random Planted Forest -----
+#' if (requireNamespace("randomPlantedForest", quietly = TRUE)) {
+#' library(randomPlantedForest)
+#' rp <- rpf(mpg ~ hp + wt + drat, data = mtcars[1:26, ], max_interaction = 2)
+#'
+#' glex(rp, mtcars[27:32, ])
+#' }
 print.glex <- function(x, ...) {
 
   n <- nrow(x$x)
   n_m <- ncol(x$m)
 
-
   cat("glex object of subclass", class(x)[[2]], "\n")
-  cat("Explaining", n, "observations with", n_m, "terms:\n\n")
+  cat("Explaining predictions of", n, "observations with", n_m, "terms:\n\n")
   #cat("Components $m containing", n_m, "terms :\n")
   str(x, list.len = 5)
 }
