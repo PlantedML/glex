1057d968c5b8720a3de85acf8d3398ee111fb924
Lukas Burk <lukas@quantenbrot.de>
2023-03-05 02:24:47 +0100

    Add print.glex

---
+++ R/print.R
@@ -0,0 +1,21 @@
+#' Print glex objects
+#'
+#' This is implemented mainly to avoid flooding the console in cases where the `glex` object
+#' uses many terms, which leads to a large amount of column names of `$m` being printed to the console.
+#' This function wraps [`str()`][utils::str] with a truncated output for a more compact representation.
+#' @param x Object to print
+#' @param ... (Unused)
+#'
+#' @export
+#' @importFrom utils str
+print.glex <- function(x, ...) {
+
+  n <- nrow(x$x)
+  n_m <- ncol(x$m)
+
+
+  cat("glex object of subclass", class(x)[[2]], "\n")
+  cat("Explaining", n, "observations with", n_m, "terms:\n\n")
+  #cat("Components $m containing", n_m, "terms :\n")
+  str(x, list.len = 5)
+}
