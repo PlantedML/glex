e98d67f221a0fdfa1c86b05f0759690e0d19b84f
Lukas Burk <jemus42@users.noreply.github.com>
2023-11-13 11:05:42 +0100

    chore: add plot_pdp test

---
+++ tests/testthat/test-plot_pdp.R
@@ -0,0 +1,36 @@
+# These tests only ensure that glex can create a ggplot without error
+# Plots still need to be manually inspected {vdiffr} would be an option but would only make sense
+# once actual plot appearance is finalized.
+
+# Regression / rpf ------------------------------------------------------------------------------------------------
+test_that("regression rpf", {
+  skip_if_not_installed("randomPlantedForest")
+  rp <- rpf(mpg ~ cyl + hp + wt, data = mtcars, max_interaction = 3)
+  gl <- glex(rp, mtcars)
+
+  p <- plot_pdp(gl, "wt")
+  expect_s3_class(p, "ggplot")
+})
+
+# Binary / rpf ------------------------------------------------------------------------------------------------------
+test_that("binary rpf", {
+  skip_if_not_installed("randomPlantedForest")
+  rp <- rpf(y ~ x1 + x2 + x3, data = xdat, max_interaction = 3)
+  gl <- glex(rp, xdat)
+
+  p <- plot_pdp(gl, "x1")
+  expect_s3_class(p, "ggplot")
+
+  expect_identical(p, autoplot(gl, "x1"))
+})
+
+
+# Multiclass / rpf ------------------------------------------------------------------------------------------------
+test_that("multiclass rpf", {
+  skip_if_not_installed("randomPlantedForest")
+  rp <- rpf(yk ~ x1 + x2 + x3, data = xdat, max_interaction = 3)
+  gl <- glex(rp, xdat)
+
+  p <- plot_pdp(gl, "x1")
+  expect_s3_class(p, "ggplot")
+})
