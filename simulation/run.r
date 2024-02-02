d808b07a290f25559d99846f8af6875c56c4ee94
Jinyang Liu <jl@math.ku.dk>
2024-02-02 11:58:36 +0100

    Register for parallel do

--- simulation/run.r
+++ simulation/run.r
@@ -1,8 +1,9 @@
 library(future)
 library(future.apply)
+library(doFuture)
 
+registerDoFuture()
 plan(multicore)
-
 source("main_loop.r")
 
 # Read command-line arguments
@@ -17,11 +18,9 @@ loaded_params <- read.csv("params.csv")
 
 sim_params <- loaded_params[line_num, ]
 
-print("Simulating using parameters:")
-print(sim_params)
-print("For this many iterations:")
-print(nsim)
+print(paste("Simulating using parameters:", sim_params))
+print(paste("For this many iterations:", nsim))
 
 
 sim_res <- simulate_for_B(sim_params$n, sim_params$c, sim_params$s, B = nsim)
-save(sim_res, paste0('sim_res', sim_params$n, sim_params$c, sim_params$s, '.RData'))
+save(sim_res, file = paste0('sim_res', sim_params$n, sim_params$c, sim_params$s, '.RData'))
