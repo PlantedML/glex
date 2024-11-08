e319fb2695d01462ac05f360c2e56443914a9324
Jinyang Liu <jl@math.ku.dk>
2024-11-08 16:56:11 +0100

    Add FastPD C++ implementation

---
+++ inst/include/glex.h
@@ -0,0 +1,18 @@
+
+#include <set>
+#include <map>
+#include <vector>
+
+
+typedef std::map<std::set<unsigned int>, double> ProbsMap;
+typedef std::map<std::set<unsigned int>, std::vector<unsigned int>> PathData;
+
+struct AugmentedData {
+    std::set<unsigned int> encountered;
+    PathData pathData;
+};
+
+struct LeafData {
+    std::map<unsigned int, std::set<unsigned int>> encountered;
+    std::map<unsigned int, ProbsMap> leafProbs;
+};
