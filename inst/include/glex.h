e08c03fa37fa492d8aaa40a358cae171b74351fa
Jinyang Liu <jl@math.ku.dk>
2024-11-12 14:36:38 +0100

    Indent with 2 spaces

--- inst/include/glex.h
+++ inst/include/glex.h
@@ -11,24 +11,24 @@ typedef std::map<std::set<unsigned int>, std::vector<unsigned int>> PathData;
 
 struct AugmentedData
 {
-    std::set<unsigned int> encountered;
-    PathData pathData;
+  std::set<unsigned int> encountered;
+  PathData pathData;
 };
 
 struct LeafData
 {
-    std::set<unsigned int> all_encountered;
-    std::map<unsigned int, std::set<unsigned int>> encountered;
-    std::map<unsigned int, ProbsMap> leafProbs;
+  std::set<unsigned int> all_encountered;
+  std::map<unsigned int, std::set<unsigned int>> encountered;
+  std::map<unsigned int, ProbsMap> leafProbs;
 };
 
 enum Index
 {
-    FEATURE = 0,
-    SPLIT = 1,
-    YES = 2,
-    NO = 3,
-    QUALITY = 4,
+  FEATURE = 0,
+  SPLIT = 1,
+  YES = 2,
+  NO = 3,
+  QUALITY = 4,
 };
 
 #endif // GLEX_H_
