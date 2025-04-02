ca33087d2ee4d7404fd63b2dc18e4e541b66d347
Lukas Burk <jemus42@users.noreply.github.com>
2025-04-02 15:13:33 +0200

    Fix test by directly referencing stats functions (#30)
    * Fix missing stats:: import
    
    * Revert trying to run tests in parallel for safety
    
    * Remove snapshot test in print test
    
    * Cleanup

--- tests/testthat/test-print-glex.R
+++ tests/testthat/test-print-glex.R
@@ -1,12 +1,13 @@
 test_that("print.glex works", {
   set.seed(2)
-  rp <- rpf(mpg ~ cyl, data = mtcars)
+  rp <- rpf(mpg ~ cyl + hp, data = mtcars)
   gl <- glex(rp, mtcars)
 
   out <- capture.output(gl)
 
   expect_match(out[[1]], "^glex object of subclass rpf_components")
-
-  # snapshot test maybe?
-  expect_snapshot(out)
+  expect_match(
+    out[[2]],
+    "^Explaining predictions of 32 observations with 2 terms of up to 1 degree"
+  )
 })
